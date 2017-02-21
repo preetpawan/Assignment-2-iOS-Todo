d06a00f0eaee5b6633ca371cc19129a1a1ef56b0
pawanpreet kaur <pawanpreetkaur@pawanpreets-mac.local>
2017-02-20 19:43:08 -0500

    initial commit

--- ToDo_pawanpreet300916703/update.swift
+++ ToDo_pawanpreet300916703/update.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  update.swift
 //  ToDo_pawanpreet300916703
 //
 //  Created by pawanpreet kaur on 2017-02-20.
@@ -10,32 +10,43 @@ import UIKit
 import UIKit
 import Firebase
 import FirebaseDatabase
-class ViewController: UIViewController {
 
+
+class update: UIViewController {
+    
     @IBOutlet weak var textbox1: UITextField!
     @IBOutlet weak var textbox2: UITextView!
+     var pojoo : pojo!
     override func viewDidLoad() {
+        
         super.viewDidLoad()
+        textbox1.text = pojoo.txt1
+        textbox2.text = pojoo.txt2
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     var databaseref : FIRDatabaseReference!
     {
         return FIRDatabase.database().reference()
     }
-
-    @IBAction func add(_ sender: Any) {
-        
-        let todoref = databaseref.child("todo").childByAutoId()
+    
+    @IBAction func update(_ sender: Any) {
+     //   let todoref = databaseref.child("todo").childByAutoId()
         
         let text1 = textbox1.text
         let text2 = textbox2.text
-        let pojoobj = pojo(txt1: text1!, txt2: text2!)
-        todoref.setValue(pojoobj.toAnyObject())
-        //self.navigationController?.popToViewController(<#T##viewController: UIViewController##UIViewController#>, animated: <#T##Bool#>)
+        
+        let updatepojo = pojo(txt1: text1!, txt2: text2!)
+        
+        let key = pojoo.ref!.key
+        
+        let updateref = databaseref.child("/todo/\(key)/")
+        updateref.updateChildValues(updatepojo.toAnyObject())
         self.navigationController?.popViewController(animated: true)
-        // self.navigationController?.popToViewControllerAnimated(true)
+      
+        self.navigationController?.popViewController(animated: true)
+        
     }
-
+    
 }
-
